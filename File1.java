61bf4fe15aecbdaef542751c18e691c2eb8a57f0
yvpriyatham <venkatpriyatham123@gmail.com>
2024-05-16 17:07:59 +0530

    added method

--- File1.java
+++ File1.java
@@ -4,9 +4,16 @@ public class File1
 {
   int x=1000;
   int y=2000;
+
+  public void add(){
+	  System.out.println(x+y);
+  }
+
 public static void main(String[]args){
 System.out.println("First Java");
 System.out.println("this is priyatham");
 System.out.println("this is dhoni");
+File1 new = file1()
+
 }
 }
